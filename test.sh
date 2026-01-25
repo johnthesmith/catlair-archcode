19d46bb48e37b986147aad65a15102629b691e2f
Still Swamp <you@example.com>
2026-01-25 19:15:48 +0300

    Auto commit

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@
 --engine.payload=archcode \
 --engine.method=build \
 --source=../../scraps/arch-source \
---destination-path=../../scraps \
+--destination-path=../../scraps/arch-dest \
 --project-path=arch \
 --index-file=index.md
